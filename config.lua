22fb25a60cd4b0b448b9d8dccf6f5a8d4f1f543d
JUJUx1 <bs4729926@gmail.com>
2025-10-15 13:14:49 +0545

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,21 @@
+-- config.lua - USER SETTINGS
+return {
+    -- Aiming Settings
+    MaxDistance = 500,
+    Smoothness = 0.8,
+    Prediction = true,
+    
+    -- Visual Settings  
+    ESPColor = {255, 0, 0},
+    TargetColor = {255, 255, 0},
+    ESPTransparency = 0.5,
+    
+    -- Toggle Defaults
+    DefaultAimbot = true,
+    DefaultESP = true,
+    DefaultTeamCheck = false,
+    
+    -- UI Settings
+    UIPosition = {0.5, -150, 0.1, 0},
+    UISize = {300, 400}
+}
